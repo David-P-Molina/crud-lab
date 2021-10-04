809231853d8c4820e0d914a729b1d500bbfc186e
David Moina <davemolina777@gmail.com>
2021-10-04 11:40:55 -0500

    commit tree 9bfa2cad0207dd0adc0f43bd0dff5673a1b3b6fc

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -3,9 +3,28 @@ import Reviews from './Reviews';
 
 class ReviewInput extends Component {
   render() {
+    state = {
+      text: '',
+    }
+    handleOnChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+    handleOnSubmit = (e) => {
+      e.preventDefault()
+      this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId })
+      this.setState({
+        text: '',
+      })
+    }
     return (
       <div>
-        Review Input
+       <form onSubmit={this.handleOnSubmit}>
+        <label htmlFor='review-content'>Review: </label>
+        <input type="text" name="text" value={this.state.text} onChange={this.handleOnChange} />
+        <input type='submit' />
+       </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,31 @@
 import cuid from 'cuid';
+export const cuidFn = cuid
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: []
+    } , action) {
+        switch (action.type) {
+            case 'ADD_RESTAURANT':
+                const restaurant = { text: action.text, id: cuidFn() }
+                return {
+                    ...state, 
+                    restaurants:
+                        [ ...state.restaurants, restaurant ] 
+                    }
+            case 'DELETE_RESTAURANT': 
+                const restaurants = state.restaurants.filter((rest) => rest.id !== action.id )
+                return {
+                    ...state,
+                    restaurants
+                }
+            // case 'ADD_REVIEW':
+            //     return {
+            //     }
+            // case 'DELETE_REVIEW':
+            //     return {
+            //     }
+            default: 
+            return state
+        }
 }
